7eec2d263b844e90df0287dd6cd9ade98b4cd3e6
elam1234 <152494447+elam1234@users.noreply.github.com>
2023-12-05 10:48:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,93 @@
+resource "aws_vpc" "elam" {
+  cidr_block = var.cidr
+}
+resource "aws_subnet" "sub1" {
+  vpc_id     = aws_vpc.elam.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  }
+  resource "aws_subnet" "sub2" {
+  vpc_id     = aws_vpc.elam.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+  }
+  resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.elam.id
+  }
+  resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.elam.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  }
+  resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.rt.id
+}
+ resource "aws_route_table_association" "rta2" {
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.rt.id
+}
+resource "aws_security_group" "sg" {
+  name        = "sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.elam.id
+
+  ingress {
+    description      = "http from vpc"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "port ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+resource "aws_s3_bucket" "elam1" {
+  bucket = "elakeerthuloveupannavendamvenumaaaa"
+}
+
+resource "aws_instance" "webserver1" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.Sg.id]
+  subnet_id              = aws_subnet.sub1.id
+  user_data              = base64encode(file("userdata.sh"))
+}
+
+resource "aws_instance" "webserver2" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.sub2.id
+  user_data              = base64encode(file("userdata1.sh"))
+}
 resource "aws_lb" "myalb" {
   name               = "myalb"
   internal           = false
   load_balancer_type = "application"
 
-  security_groups = [aws_security_group.webSg.id]
+  security_groups = [aws_security_group.Sg.id]
   subnets         = [aws_subnet.sub1.id, aws_subnet.sub2.id]
 
   tags = {
@@ -15,7 +99,7 @@ resource "aws_lb_target_group" "tg" {
   name     = "myTG"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.myvpc.id
+  vpc_id   = aws_vpc.elam.id
 
   health_check {
     path = "/"
